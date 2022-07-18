aaf3accb848b397b6c8e9d669293d945c1f99031
Denys <denis4242503@gmail.com>
2022-07-18 20:36:23 +0300

    місце інтересу

---
+++ SwanQuest/Assets/UI/Scripts/ArrowHoldEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ArrowHoldEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	[SerializeField] private Image _img; 
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		_img.gameObject.SetActive(true);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		_img.gameObject.SetActive(false);
+	}
+}
---
+++ SwanQuest/Assets/UI/Scripts/CameraController1.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+
+public class CameraController1 : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+	private enum Side
+	{
+	
+		left,
+		right,
+		top,
+		bottom
+	}
+
+    [SerializeField] private Side _side;
+	[SerializeField] private float _speed;
+	[SerializeField] private float _step;
+	[SerializeField] private float _constrain_coordinate;
+	[SerializeField] private Camera _camera;
+ 
+	private bool MouseButtonHeldDown = false;
+	public void OnPointerDown(PointerEventData eventData)
+	{
+ 	   
+ 	      MouseButtonHeldDown  = true ;
+ 	}
+ 	public void OnPointerUp(PointerEventData eventData)
+ 	{
+ 	      MouseButtonHeldDown = false;
+ 	}
+	public void Update() 
+	{
+		if(!MouseButtonHeldDown)
+			return;
+		var time = _speed * Time.deltaTime;
+		var x_position = _camera.transform.position.x;
+		var y_position = _camera.transform.position.y;
+		var z_position = _camera.transform.position.z;
+		var local_position = new Vector3(x_position, y_position, z_position);
+		var global_position = transform.TransformPoint(local_position);
+
+		Debug.Log($"{x_position}, {y_position}");
+		//Debug.Log(global_position.x);
+		switch(_side)
+		{
+
+			case Side.left:
+				x_position = x_position > _constrain_coordinate ?
+					Mathf.Lerp(x_position, x_position - _step, time) : x_position;
+				break;
+			case Side.right:
+				x_position = x_position < _constrain_coordinate ? 
+					Mathf.Lerp(x_position, x_position + _step, time) : x_position;
+				break;
+			case Side.top:
+				y_position = y_position < _constrain_coordinate ? 
+					Mathf.Lerp(y_position, y_position + _step, time) : y_position;
+				break;
+			case Side.bottom:
+				y_position = y_position > _constrain_coordinate ? 
+					Mathf.Lerp(y_position, y_position - _step, time) : y_position;
+				break;
+		}
+
+
+		_camera.transform.position = new Vector3(x_position, y_position, z_position);
+
+	//	_camera.transform.position = new Vector3(10, 0, 0);
+	}
+}
